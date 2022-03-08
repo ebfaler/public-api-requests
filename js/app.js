75f09527b9308898457e2949766becbd7f6999f0
ebfaler <74653573+ebfaler@users.noreply.github.com>
2022-03-08 16:22:21 +0100

    added click event for card

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
+
+//global variables
 let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`
 const headerContainer = document.querySelector(".header-inner-container");
+const searchBox = document.querySelector(".search-input");
 const gallery = document.querySelector(".gallery");
 const modalContainer = document.querySelector(".modal-container");
-
-// const cardContainer = document.querySelector(".card");
-
-
+modalContainer.style.display = "none";
 const modalClose = document.querySelector(".modal-close-btn")
-const searchBox = document.querySelector(".search-input");
+const modalBtn = document.querySelectorAll('modal-btn-container .btn');
 const previous = document.querySelector(".modal-prev");
 const next = document.querySelector(".modal-next");
 let modalIndex = 0;
+
 
 
 // fetch data from API
@@ -26,7 +27,7 @@ fetch(urlAPI)
 
 function displayEmployees(employeeData) {
     employees = employeeData; //setting employees variable equal to employeeData so that it can be accessed outside of this function
- 
+
     // store the employee HTML as we create it
     let employeeHTML = '';
 
@@ -39,9 +40,9 @@ function displayEmployees(employeeData) {
         let picture = employee.picture;
 
         // template literals make this so much cleaner!
-        employeeHTML += 
+        employeeHTML +=
 
-    `<div class="card">
+ `<div class="card" data-index="${index}">
     <div class="card-img-container">
         <img class="card-img" src="${picture.large}" alt="profile picture">
     </div>
@@ -50,7 +51,7 @@ function displayEmployees(employeeData) {
         <p class="card-text">${email}</p>
         <p class="card-text cap">${city}, ${state}</p>
     </div>
-    </div>`
+ </div>`
     });
     gallery.innerHTML = employeeHTML;
 }
@@ -58,29 +59,55 @@ function displayEmployees(employeeData) {
 //display modal
 function displayModal(index) {
     // use object destructuring make our template literal cleaner
-    console.log(index);
+    // console.log(index);
     let { name, dob, phone, email, location: { city, street, state, postcode
     }, picture } = employees[index];
 
     let date = new Date(dob.date);
 
     const modalHTML = `
-    <img class="avatar" src="${picture.large}" />
-    <div class="text-container">
-    <h2 class="name">${name.first} ${name.last}</h2>
-    <p class="email">${email}</p>
-    <p class="address">${city}</p>
-    <hr />
-    <p>${phone}</p>
-    <p class="address">${street.number}, ${street.name}, ${state}, ${postcode}</p>
-    <p>Birthday:
-    ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+               <img class="avatar" src="${picture.large}" />
+               <div class="text-container">
+               <h2 class="name">${name.first} ${name.last}</h2>
+               <p class="email">${email}</p>
+               <p class="address">${city}</p>
+               <hr />
+               <p>${phone}</p>
+               <p class="address">${street.number}, ${street.name}, ${state}, ${postcode}</p>
+               <p>Birthday:
+               ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+            </div>
+        </div>
+
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
     </div>
     `;
 
-
-    overlay.classList.remove("hidden");
+    modalContainer.style.display ="block";
     modalContainer.innerHTML = modalHTML;
     modalIndex = index;
 
 };
+
+
+
+//event listener to load modal on card click
+
+gallery.addEventListener("click", (e) => {
+
+    if (e.target !== gallery) {
+         // select the card element based on its proximity to element clicked
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+        displayModal(index);
+        console.log("card clicked");
+    }
+
+});
